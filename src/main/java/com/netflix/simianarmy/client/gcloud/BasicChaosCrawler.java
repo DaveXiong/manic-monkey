7d4a3b3551a6926835b9d41269fd8e30fbb2c310
Dave Xiong <xionghaitao.org@gmail.com>
2016-11-01 15:23:11 +1000

    integration with GCE

---
+++ src/main/java/com/netflix/simianarmy/client/gcloud/BasicChaosCrawler.java
@@ -0,0 +1,83 @@
+package com.netflix.simianarmy.client.gcloud;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netflix.simianarmy.GroupType;
+import com.netflix.simianarmy.chaos.ChaosCrawler;
+import com.netflix.simianarmy.client.gcloud.Gce.Instance;
+
+/**
+ * The Class BasicChaosCrawler.
+ */
+public class BasicChaosCrawler implements ChaosCrawler {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(BasicChaosCrawler.class);
+
+	/**
+	 * The group types Types.
+	 */
+	public enum Types implements GroupType {
+		ASG, TAG
+	}
+
+	/** The gce client. */
+	private final BasicClient client;
+
+	/**
+	 * Instantiates a new basic chaos crawler.
+	 * 
+	 * @param client
+	 *            the aws client
+	 */
+	public BasicChaosCrawler(BasicClient client) {
+		this.client = client;
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public EnumSet<?> groupTypes() {
+		return EnumSet.allOf(Types.class);
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public List<InstanceGroup> groups() {
+		return groups((String[]) null);
+	}
+
+	@Override
+	public List<InstanceGroup> groups(String... names) {
+		List<InstanceGroup> list = new LinkedList<InstanceGroup>();
+
+		List<String> groupNames = new ArrayList<String>();
+		if (names != null)
+			groupNames.addAll(Arrays.asList(names));
+		try {
+			list.addAll(client.listGroups());
+
+			for (InstanceGroup group : list) {
+				if (groupNames.isEmpty() || groupNames.contains(group.name())) {
+					try {
+						for (Instance instance : client.list(group.name())) {
+							group.addInstance(instance.getName());
+						}
+					} catch (IOException ex) {
+						LOGGER.error(ex.getMessage());
+					}
+				}
+			}
+
+		} catch (IOException ex) {
+			LOGGER.error(ex.getMessage());
+		}
+		return list;
+	}
+}
---
+++ src/main/java/com/netflix/simianarmy/client/gcloud/Definitions.java
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+package com.netflix.simianarmy.client.gcloud;
+
+import com.netflix.simianarmy.basic.BasicCalendar;
+import com.netflix.simianarmy.basic.LocalDbRecorder;
+import com.netflix.simianarmy.basic.chaos.BasicChaosInstanceSelector;
+
+/**
+ * @author dxiong
+ *
+ */
+public interface Definitions {
+
+	public interface GCloud {
+		String[] CONFIG = new String[] { "simianarmy.properties", "client.properties", "chaos.properties" };
+		String CRENDENTIAL = "simianarmy.client.gcloud.credential";
+		String PROJECT = "simianarmy.client.gcloud.project";
+		String ZONE = "simianarmy.client.gcloud.zone";
+		String ZONE_DEFAULT = "us-east-1";
+	}
+
+	public interface Client {
+		String CLASS = "simianarmy.client.class";
+		String CLASS_DEFAULT = com.netflix.simianarmy.client.gcloud.BasicClient.class.getName();
+	}
+
+	public interface Scheduler {
+		String FREQUEUE = "simianarmy.scheduler.frequency";
+		int FREQUEUE_DEFAULT = 1;
+		String FREQUEUE_UNIT = "simianarmy.scheduler.frequencyUnit";
+		String FREQUEUE_UNIT_DEFAULT = "HOURS";
+		String THREADS = "simianarmy.scheduler.threads";
+		int THREADS_DEFAULT = 1;
+	}
+
+	public interface Recorder {
+		String CLASS = "simianarmy.client.recorder.class";
+		String CLASS_DEFAULT = LocalDbRecorder.class.getName();
+
+		public interface RDS {
+			String DRIVER = "simianarmy.recorder.db.driver";
+			String URL = "simianarmy.recorder.db.url";
+			String USER = "simianarmy.recorder.db.user";
+			String PASSWORD = "simianarmy.recorder.db.pass";
+			String TABLE = "simianarmy.recorder.db.table";
+		}
+	}
+
+	public interface Calendar {
+		String CLASS = "simianarmy.calendar.class";
+		String CLASS_DEFAULT = BasicCalendar.class.getName();
+	}
+
+	public interface InstanceSelector {
+		String CLASS = "simianarmy.client.instance.selector.class";
+		String CLASS_DEFAULT = BasicChaosInstanceSelector.class.getName();
+	}
+
+	public interface Notifier {
+		public interface Email {
+			String CLASS = "simianarmy.client.notifier.email.class";
+			String CLASS_DEFAULT = DummyMonkyEmailNotifier.class.getName();
+		}
+
+		public interface SLACK {
+			String URL = "simianarmy.client.notifier.slack.url";
+			String CHANNEL = "simianarmy.client.notifier.slack.channel";
+			String CHANNEL_DEFAULT = "#wfe-test";
+			String AGENT = "simianarmy.client.notifier.slack.agent";
+			String AGENT_DEFAULT = "Manic Monkey";
+		}
+
+	}
+
+	public interface Crawler {
+		String CLASS = "simianarmy.client.crawler.class";
+		String CLASS_DEFAULT = BasicChaosCrawler.class.getName();
+	}
+}
