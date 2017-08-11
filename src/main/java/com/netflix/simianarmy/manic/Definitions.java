2ae20f7083ffb9258de021964d1c520bf03c5f40
Dave Xiong <xionghaitao.org@gmail.com>
2017-08-11 13:55:56 +1000

    0.0.16

--- src/main/java/com/netflix/simianarmy/manic/Definitions.java
+++ src/main/java/com/netflix/simianarmy/manic/Definitions.java
@@ -8,7 +8,7 @@ package com.netflix.simianarmy.manic;
  *
  */
 public interface Definitions {
-	String VERSION = "0.0.15";
+	String VERSION = "0.0.16";
 
 	long UP_AT = System.currentTimeMillis();
 }
